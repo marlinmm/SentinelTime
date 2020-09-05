a84380b2a9b0a06b6fd65b60d6c0e6d45d8883d4
marlinmm <marlinmueller.mm@gmail.com>
2020-09-05 21:49:38 +0200

    add extract_time_series function

--- SentinelTime/mask_stack.py
+++ SentinelTime/mask_stack.py
@@ -1,18 +1,20 @@
-import fiona
-import rasterio as rio
 import rasterio.mask
-import numpy as np
 import shutil
 import rasterio.plot
 from SentinelTime.data_preprocessing import *
 
+
 def mask_tif(shape_path, sen_directory):
+    """
+
+    :param shape_path:
+    :param sen_directory:
+    :return:
+    """
     file_list = extract_files_to_list(path_to_folder=sen_directory, datatype=".tif", path_bool=True)
     file_name_list = extract_files_to_list(path_to_folder=sen_directory, datatype=".tif", path_bool=False)
     shapes = import_polygons(shape_path=shape_path)
-    print(file_list)
     print(shapes)
-
     masked_folder = sen_directory + "masked/"
     if os.path.exists(masked_folder):
         shutil.rmtree(masked_folder)
@@ -21,20 +23,21 @@ def mask_tif(shape_path, sen_directory):
     for i, files in enumerate(file_list):
         src1 = rio.open(file_list[i])
         out_image, out_transform = rio.mask.mask(src1, [shapes[0]], all_touched=0, crop=True, nodata=np.nan)
-        # print(np.shape(src1))
-        # print(np.shape(out_image))
         out_meta = src1.meta
         out_meta.update({"driver": "GTiff",
                          "height": out_image.shape[1],
                          "width": out_image.shape[2],
                          "transform": out_transform})
-
         with rasterio.open(
                 masked_folder + file_name_list[i][0:len(file_name_list)-4] + "_masked.tif", "w", **out_meta) as dest:
             dest.write(out_image)
 
 
 def raster_stack(sen_directory):
+    """
+    :param sen_directory:
+    :return:
+    """
     file_list = extract_files_to_list(path_to_folder=sen_directory + "masked/", datatype=".tif", path_bool=True)
 
     # Read metadata of first file
@@ -45,7 +48,31 @@ def raster_stack(sen_directory):
     meta.update(count=len(file_list))
 
     # Read each layer and write it to stack
-    with rasterio.open(sen_directory + 'masked/stack.tif', 'w', **meta) as dst:
+    with rasterio.open(sen_directory + 'AAA_stack.tif', 'w', **meta) as dst:
         for id, layer in enumerate(file_list, start=1):
             with rasterio.open(layer) as src1:
                 dst.write_band(id, src1.read(1))
+
+
+def extract_time_series(layer_stack, point_path, buffer_size):
+    """
+
+    :param buffer_size:
+    :param layer_stack:
+    :param point_path:
+    :return:
+    """
+    patches = create_point_buffer(point_path, buffer_size=buffer_size)
+    src1 = rio.open(layer_stack)
+    patch_mean = []
+    for patch in patches:
+        pixel_mean = []
+        out_image, out_transform = rio.mask.mask(src1, [patch], all_touched=0, crop=True, nodata=np.nan)
+        for pixel in out_image:
+            pixel_mean.append(np.nanmean(pixel))
+        patch_mean.append(pixel_mean)
+    print(patch_mean)
+    print(len(patch_mean))
+    print(len(patch_mean[0]))
+    print(len(patch_mean[1]))
+    return patch_mean
