3c54e8fc37e33eabc2dfd66b5cdc3985ba17fd08
marlinmm <marlinmueller.mm@gmail.com>
2020-09-05 21:38:06 +0200

    added buffer function

--- SentinelTime/data_preprocessing.py
+++ SentinelTime/data_preprocessing.py
@@ -7,8 +7,6 @@ import numpy as np
 
 def extract_files_to_list(path_to_folder, datatype, path_bool):
     """
-    finds all .tif-files in the corresponding directory
-    :return:
     """
     new_list = []
     for filename in os.listdir(path_to_folder):
@@ -24,8 +22,6 @@ def extract_files_to_list(path_to_folder, datatype, path_bool):
 
 def import_polygons(shape_path):
     """
-    imports the 3x3km polygons of the DWD weather stations
-    :return:
     """
     active_shapefile = fiona.open(shape_path, "r")
     for i in range(0,len(list(active_shapefile))):
@@ -33,11 +29,7 @@ def import_polygons(shape_path):
     return shapes
 
 
-def eliminate_nanoverlap(sen_directory, shape_path):
-    """
-    eliminates the scenes which would'nt match with all weather stations
-    :return:
-    """
+def create_shape_buffer(shape_path):
     import_list = import_polygons(shape_path=shape_path)
 
     buffer_list = []
@@ -51,7 +43,44 @@ def eliminate_nanoverlap(sen_directory, shape_path):
         buffer_coord = [[upper_left, upper_right, lower_right, lower_left, upper_left]]
         buffer = {"type": "Polygon", "coordinates": buffer_coord}
         buffer_list.append(buffer)
+    return buffer_list
 
+
+def create_point_buffer(point_path):
+    """
+
+    :param point_path:
+    :return:
+    """
+    import_list = import_polygons(shape_path=point_path)
+
+    buffer_list = []
+    print(len(import_list))
+    print(import_list)
+    print(import_list[1]["coordinates"])
+    for i in range(0, len(import_list)):
+        lon = import_list[i]["coordinates"][0]
+        lat = import_list[i]["coordinates"][1]
+        print(lon)
+        upper_left = (lon - 100, lat + 100)
+        upper_right = (lon + 100, lat + 100)
+        lower_left = (lon - 100, lat - 100)
+        lower_right = (lon + 100, lat - 100)
+        buffer_coord = [[upper_left, upper_right, lower_right, lower_left, upper_left]]
+        buffer = {"type": "Polygon", "coordinates": buffer_coord}
+        buffer_list.append(buffer)
+    print(buffer_list)
+    return buffer_list
+
+
+def eliminate_nanoverlap(sen_directory, shape_path):
+    """
+
+    :param sen_directory:
+    :param shape_path:
+    :return:
+    """
+    buffer_list = create_shape_buffer(shape_path=shape_path)
     file_list = extract_files_to_list(path_to_folder=sen_directory, datatype=".tif", path_bool=False)
 
     for i, files in enumerate(file_list):
@@ -70,5 +99,5 @@ def eliminate_nanoverlap(sen_directory, shape_path):
         if test_bool == 4:
             # os.rename(sen_directory + file_list[i], sen_directory + file_list[i][0:len(file_list[i])-4] + "_overlap.tif")
             # os.rename(sen_directory + file_list[i], sen_directory + file_list[i][0:len(file_list[i]) - 12] + ".tif")
-            # print(file_list[i])
-            os.rename(sen_directory + file_list[i], sen_directory + file_list[i][10:len(file_list[i])])
+            print(file_list[i])
+            # os.rename(sen_directory + file_list[i], sen_directory + file_list[i][10:len(file_list[i])])
