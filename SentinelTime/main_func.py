59ba0c47ed60d3619931bbd5b7f38b9ac62cb733
marlinmm <marlinmueller.mm@gmail.com>
2020-09-05 18:57:06 +0200

    cleaned up main func

--- SentinelTime/main_func.py
+++ SentinelTime/main_func.py
@@ -1,41 +1,29 @@
 import fiona
+import rasterio as rio
 import rasterio.mask
 import numpy as np
-from osgeo import gdal
+import shutil
 import rasterio.plot
 from SentinelTime.data_preprocessing import *
-
-
-def eliminate_non_overlap(sen_directory, shape_path):
-    eliminate_nanoverlap(sen_directory, shape_path)
-
-def mask_tiff ():
-    with fiona.open("G:/Shapes/Polygons/extend_of_points.shp",
-                "r") as shapefile:
-        shapes = [feature["geometry"] for feature in shapefile]
-        shapes = np.array_split(shapes, 89)
-        with rasterio.open(r'/geonfs03_vol1/01_DMC_South_Africa/06_ORTHO/01_KNP/02_25cm/vrt/KNP_25cm__compressed_mosaic.vrt') as raster_vrt:
-            # rasterio.plot.show(raster_vrt)
-            out_image, out_transform = rasterio.mask.mask(raster_vrt, shapes[82], crop=True)
-            out_meta = raster_vrt.meta
-
-            # Driver: "VRT" or "GTiff"?
-            out_meta.update({"driver": "GTiff",
-                             "height": out_image.shape[1],
-                             "width": out_image.shape[2],
-                             "transform": out_transform})
-
-        with rasterio.open(
-                "/home/ni82xoj/Ortho_SALDI/Outtest/Station_masked"
-                + str(82) + ".tif", "w", **out_meta) as dest:
-            dest.write(out_image)
+from SentinelTime.mask_stack import *
 
 def main():
-    sen_directory = "G:/Processed/processed_existing/VH"
-    sen_overlap_dir = "G:/Processed/processed_existing/VH_overlap"
-    shape_path = "G:/Shapes/Polygons/extend_of_points.shp"
-    #eliminate_non_overlap(sen_directory=sen_directory, shape_path=shape_path)
+    # sen_directory = "G:/Processed/processed_existing/VH"
+    # sen_overlap_dir = "G:/Processed/processed_existing/VH_overlap/"
 
+    sen_directory = "G:/Processed/processed_new_per_year/VH/"
+    sen_overlap_dir = "G:/Processed/processed_new_per_year/VH_overlap/"
+
+    VH_asc = "G:/Processed/preprocessd/VH_overlap/Asc/"
+    VH_desc = "G:/Processed/preprocessd/VH_overlap/Desc/"
+
+    shape_path = "G:/Shapes/Polygons/extend_of_points.shp"
+
+
+    # eliminate_nanoverlap(sen_directory=sen_directory, shape_path=shape_path)
+    # eliminate_non_overlap(sen_directory=sen_overlap_dir, shape_path=shape_path)
+    # mask_tif(shape_path=shape_path, sen_directory=VH_asc)
+    raster_stack(sen_directory=VH_asc)
 
 
 if __name__ == '__main__':
