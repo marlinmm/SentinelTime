dcf8e0835857f8a4871684de505063655a84c802
marlinmm <marlinmueller.mm@gmail.com>
2020-09-05 21:49:07 +0200

    add paths and functions to main

--- SentinelTime/main_func.py
+++ SentinelTime/main_func.py
@@ -1,10 +1,3 @@
-import fiona
-import rasterio as rio
-import rasterio.mask
-import numpy as np
-import shutil
-import rasterio.plot
-from SentinelTime.data_preprocessing import *
 from SentinelTime.mask_stack import *
 
 def main():
@@ -18,12 +11,16 @@ def main():
     VH_desc = "G:/Processed/preprocessd/VH_overlap/Desc/"
 
     shape_path = "G:/Shapes/Polygons/extend_of_points.shp"
+    point_path = "G:/Shapes/Points/clc111_reproj.shp"
 
+    layer_stack = "G:/Processed/preprocessd/VH_overlap/Asc/AAA_stack.tif"
 
     # eliminate_nanoverlap(sen_directory=sen_directory, shape_path=shape_path)
-    # eliminate_non_overlap(sen_directory=sen_overlap_dir, shape_path=shape_path)
+    # eliminate_nanoverlap(sen_directory=sen_overlap_dir, shape_path=shape_path)
     # mask_tif(shape_path=shape_path, sen_directory=VH_asc)
-    raster_stack(sen_directory=VH_asc)
+    # raster_stack(sen_directory=VH_asc)
+    # create_point_buffer(point_path=point_path, buffer_size=100)
+    extract_time_series(layer_stack=layer_stack, point_path=point_path, buffer_size=100)
 
 
 if __name__ == '__main__':
