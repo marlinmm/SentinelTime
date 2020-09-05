dae703b1421f82c5c751e0dfdb0d0e7ebcda7bd1
marlinmm <marlinmueller.mm@gmail.com>
2020-09-05 21:49:16 +0200

    moar tests

--- SentinelTime/test.py
+++ SentinelTime/test.py
@@ -29,3 +29,7 @@ for j in range(0,3):
 
 test_name = "G:/Processed/processed_existing/VH_overlap\S1A__IW___A_20171028T165948_VH_grd_mli_norm_geo_db_overlap.tif"
 print(test_name[12:len(test_name)])
+
+
+
+correct_shape = "[{'type': 'Polygon', 'coordinates': [[(661297.9576407432, 5652850.307079568), (755797.7931697434, 5656841.327960808), (719412.666978868, 5602745.2659186805), (663535.3048818082, 5603920.857014434), (661297.9576407432, 5652850.307079568)]]}]"
