6cea2406e8093d09eb892eaa28f1f3e68e3152c3
marlinmm <marlinmueller.mm@gmail.com>
2020-09-08 18:01:03 +0200

    added new functions

--- SentinelTime/main_func.py
+++ SentinelTime/main_func.py
@@ -11,17 +11,18 @@ def main():
     VH_desc = "G:/Processed/preprocessd/VH_overlap/Desc/"
 
     shape_path = "G:/Shapes/Polygons/extend_of_points.shp"
-    point_path = "G:/Shapes/Points/clc111_reproj.shp"
+    point_path = "G:/Shapes/Points/clc312_reproj.shp"
 
-    layer_stack = "G:/Processed/preprocessd/VH_overlap/Asc/AAA_stack.tif"
+    layer_stack = "G:/Processed/preprocessd/VH_overlap/Asc/export/AAA_stack.tif"
 
     # eliminate_nanoverlap(sen_directory=sen_directory, shape_path=shape_path)
     # eliminate_nanoverlap(sen_directory=sen_overlap_dir, shape_path=shape_path)
     # mask_tif(shape_path=shape_path, sen_directory=VH_asc)
     # raster_stack(sen_directory=VH_asc)
     # create_point_buffer(point_path=point_path, buffer_size=100)
-    # extract_time_series(layer_stack=layer_stack, point_path=point_path, buffer_size=100)
-    extract_dates(sen_directory=VH_asc)
+    # extract_time_series(layer_stack=layer_stack, point_path=point_path, buffer_size=100, sen_directory=VH_asc)
+    plot_test(layer_stack=layer_stack, point_path=point_path, buffer_size=100, sen_directory=VH_asc)
+    # extract_dates(sen_directory=VH_asc)
 
 if __name__ == '__main__':
     main()
