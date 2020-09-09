6f1164f5b26c14c127fffa6fc9a0b85f433bffed
marlinmm <marlinmueller.mm@gmail.com>
2020-09-09 19:26:38 +0200

    cleaned up file

--- SentinelTime/main_func.py
+++ SentinelTime/main_func.py
@@ -1,24 +1,19 @@
 from SentinelTime.mask_stack import *
 
+
 def main():
-    # sen_directory = "G:/Processed/processed_existing/VH"
-    # sen_overlap_dir = "G:/Processed/processed_existing/VH_overlap/"
-
-    # sen_directory = "G:/Processed/processed_new_per_year/VH/"
-    # sen_overlap_dir = "G:/Processed/processed_new_per_year/VH/"
-
+    # Main directory of original raster files:
     main_dir = "G:/Processed/original_data/"
 
     # Create results folder outside of main_dir:
     results_dir = "G:/Processed/results/"
 
-    # VH_asc = "G:/Processed/preprocessd/VH_overlap/Asc/"
-    # VH_desc = "G:/Processed/preprocessd/VH_overlap/Desc/"
-
+    # Location of Shapefile to mask the ROI:
     shape_path = "G:/Shapes/Polygons/extend_of_points.shp"
+
+    # Location of the point shapefile to extract data from timeseries with:
     point_path = "G:/Shapes/Points/clc312_reproj.shp"
 
-    layer_stack = "G:/Processed/preprocessd/VH_overlap/Asc/export/AAA_stack.tif"
 
 #####------ NOT NEEDED ------#####
     # eliminate_nanoverlap(sen_directory=sen_directory, shape_path=shape_path)
@@ -33,7 +28,6 @@ def main():
     # extract_time_series(layer_stack=layer_stack, point_path=point_path, buffer_size=100, sen_directory=VH_asc)
     # plot_test(layer_stack=layer_stack, point_path=point_path, buffer_size=100, sen_directory=VH_asc)
     # extract_dates(sen_directory=VH_asc)
-
 
 
 if __name__ == '__main__':
