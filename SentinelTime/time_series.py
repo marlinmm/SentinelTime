99ecd70f3f701036469b098d04919c1eab6f2642
marlinmm <marlinmueller.mm@gmail.com>
2020-09-11 20:52:35 +0200

    added docstring asnd removed export_bool

--- SentinelTime/time_series.py
+++ SentinelTime/time_series.py
@@ -5,31 +5,33 @@ import rasterio.mask
 
 def extract_dates(directory):
     """
-
-    :param sen_directory:
-    :return:
+    Extracts dates from list of preprocessed S-1 GRD files
+    :param directory: string
+        Path to folder, where files are stored
+    :return: list
+        returns list of acquisition dates of S-1 GRD files
     """
     file_list = extract_files_to_list(path_to_folder=directory, datatype=".tif", path_bool=False)
     date_list = []
     for file in file_list:
-        # date_list.append(str(file[2:6] + "-" + file[6:8] + "-" + file[8:10]))
         date_list.append(int(file[2:10]))
     return date_list
 
 
-def extract_time_series(results_dir, point_path, buffer_size, export_bool):
+def extract_time_series(results_dir, point_path, buffer_size):
     """
-
-    :param results_dir:
-    :param point_path:
-    :param buffer_size:
-    :param export_bool:
-    :return:
+    Extracts time series information from patches of pixels using points and a buffer size to specify the size of the
+    patch
+    :param results_dir: string
+        Path to results directory, where layerstacks are stored and csv files will be stored
+    :param point_path: string
+        Path to point shapefile directory
+    :param buffer_size: int
+        Buffer size specifies the length of the rectangular buffer around the point
     """
-    np.set_printoptions(suppress=True)
+    # get
     patches = create_point_buffer(point_path, buffer_size=buffer_size)
     layer_stacks = extract_files_to_list(path_to_folder=results_dir, datatype=".tif", path_bool=True)
-    print(layer_stacks)
 
     for file in layer_stacks:
         src1 = rio.open(file)
@@ -37,10 +39,8 @@ def extract_time_series(results_dir, point_path, buffer_size, export_bool):
         for patch in patches:
             pixel_mean = []
             out_image, out_transform = rio.mask.mask(src1, [patch], all_touched=1, crop=True, nodata=np.nan)
-            # print(np.shape(out_image))
             for pixel in out_image:
                 pixel_mean.append(np.nanmean(pixel))
-            # print(pixel_mean)
             patch_mean.append(pixel_mean)
         if "VH" in file and "Asc" in file:
             patch_mean.append(extract_dates(results_dir + "VH" + "/" + "Asc" + "/"))
@@ -57,24 +57,22 @@ def extract_time_series(results_dir, point_path, buffer_size, export_bool):
         for elem in patch_mean:
             elem[0] = int(elem[0])
         src1.close()
-        if export_bool:
-            if "VH" in file and "Asc" in file:
-                np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VH_Asc.csv", patch_mean, delimiter=",",
-                           header="date,VH1,VH2,VH3,VH4,VH5,VH6,VH7,VH8,VH9,VH10,VH11,VH12,VH13,VH14,VH15,VH16,VH17,VH18,VH19,VH20",
-                           fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
-            if "VH" in file and "Desc" in file:
-                np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VH_Desc.csv", patch_mean, delimiter=",",
-                           header="date,VH1,VH2,VH3,VH4,VH5,VH6,VH7,VH8,VH9,VH10,VH11,VH12,VH13,VH14,VH15,VH16,VH17,VH18,VH19,VH20",
-                           fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
-            if "VV" in file and "Asc" in file:
-                np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VV_Asc.csv", patch_mean, delimiter=",",
-                           header="date,VV1,VV2,VV3,VV4,VV5,VV6,VV7,VV8,VV9,VV10,VV11,VV12,VV13,VV14,VV15,VV16,VV17,VV18,VV19,VV20",
-                           fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
-            if "VV" in file and "Desc" in file:
-                np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VV_Desc.csv", patch_mean, delimiter=",",
-                           header="date,VV1,VV2,VV3,VV4,VV5,VV6,VV7,VV8,VV9,VV10,VV11,VV12,VV13,VV14,VV15,VV16,VV17,VV18,VV19,VV20",
-                           fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
-    return patch_mean
+        if "VH" in file and "Asc" in file:
+            np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VH_Asc.csv", patch_mean, delimiter=",",
+                       header="date,VH1,VH2,VH3,VH4,VH5,VH6,VH7,VH8,VH9,VH10,VH11,VH12,VH13,VH14,VH15,VH16,VH17,VH18,VH19,VH20",
+                       fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
+        if "VH" in file and "Desc" in file:
+            np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VH_Desc.csv", patch_mean, delimiter=",",
+                       header="date,VH1,VH2,VH3,VH4,VH5,VH6,VH7,VH8,VH9,VH10,VH11,VH12,VH13,VH14,VH15,VH16,VH17,VH18,VH19,VH20",
+                       fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
+        if "VV" in file and "Asc" in file:
+            np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VV_Asc.csv", patch_mean, delimiter=",",
+                       header="date,VV1,VV2,VV3,VV4,VV5,VV6,VV7,VV8,VV9,VV10,VV11,VV12,VV13,VV14,VV15,VV16,VV17,VV18,VV19,VV20",
+                       fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
+        if "VV" in file and "Desc" in file:
+            np.savetxt(results_dir + point_path[point_path.index("clc"):point_path.index("clc")+6] + "_VV_Desc.csv", patch_mean, delimiter=",",
+                       header="date,VV1,VV2,VV3,VV4,VV5,VV6,VV7,VV8,VV9,VV10,VV11,VV12,VV13,VV14,VV15,VV16,VV17,VV18,VV19,VV20",
+                       fmt='%i,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f,%f')
 
 
 def import_csv(path_to_folder):
