ccdca246582a1985842b33a1b620c6246f2011cf
marlinmm <marlinmueller.mm@gmail.com>
2020-11-12 17:43:50 +0100

    added weather import for ETc stuff

--- SentinelTime/main_func.py
+++ SentinelTime/main_func.py
@@ -1,5 +1,6 @@
 from SentinelTime.mask_stack import *
 from SentinelTime.time_series import *
+from SentinelTime.weather_data import *
 
 
 def main():
@@ -11,10 +12,13 @@ def main():
     shape_path = "G:/Shapes/Polygons/new_extent.shp"
 
     # Location of the point shapefile to extract data from timeseries with (must be point shapefile!):
-    point_path = "G:/Shapes/Points/"
+    point_path = "G:/Shapes/Points"
+
+    csv_folder = "G:/Processed/results/CSV/"
 
     # Location of weatherdata in csv file format to calculate evapotranspiration:
     weather_data = "G:/Weather_data/"
+    station_heights = [442, 228, 682, 421]
 
     ###################################     OUTPUT    ########################################
     # Create results folder outside of main_dir:
@@ -25,17 +29,20 @@ def main():
     # raster_stack(shape_path=shape_path, main_dir=main_dir, results_dir=results_dir, overwrite=False)
 
     # Extract time series information based on point shapefiles and export information to csv file:
-    point_list = extract_files_to_list(path_to_folder=point_path, datatype=".shp", path_bool=True)
-    print(point_list)
-    for shapefile in point_list:
-        extract_time_series(results_dir=results_dir, shapefile=shapefile, buffer_size=100, point_path=point_path)
+    # point_list = extract_files_to_list(path_to_folder=point_path, datatype=".shp", path_bool=True)
+    # print(point_list)
+    # for shapefile in point_list:
+    #     extract_time_series(results_dir=results_dir, shapefile=shapefile, buffer_size=50, point_path=point_path)
 
     # Extract temporal statistics from time series with possibility to plot Mean and Std.Dev. values of time series for
     # each class:
-    # temporal_statistics(path_to_folder=results_dir, plot_bool=True)
+    # temporal_statistics(path_to_csv_folder=csv_folder, results_dir=results_dir, plot_bool=True)
 
     # Calculate VH/VV Ratio for each class and flight direction:
     # ratio_calc(path_to_folder=results_dir, plot_bool=True)
+
+    clean_weather_df(path_to_weather_folder=weather_data, path_to_csv_folder=csv_folder,
+                     station_heights=station_heights)
 
 
 if __name__ == '__main__':
